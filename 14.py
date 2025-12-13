9a7906c496a36ef55f686fda928c2e9c1f225d20
vanshp175-beep <vanshp175@gmail.com>
2025-12-13 12:42:40 +0530

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,88 @@
+class ATM:
+    acbal=10000
+    wcount=0
+    def validation(self):
+        pin=int(input("Enter your pin: "))
+        if pin==1234:
+            obj.viewOptions()
+        else:
+            print("Invalid pin number, try again")
+    def continue1(self):
+        value=int(input("1 for continue and 0 for EXIT"))
+        if value==1:
+            obj.viewOptions()
+        else:
+            print("Thank you, Visit again")
+    def numberOfNotes(self,amt):
+        five=0
+        two=0
+        one=1
+        if amt>=500:
+            five=amt//500
+            amt=amt-(five*500)
+            print("500 rupees notes are : ",five)
+        if amt>=200:
+            two=amt//200
+            amt=amt-(two*200)
+            print("200 rupees notes are : ",two)
+        if amt>=100:
+            print("100 rupees notes are  ",one)
+            
+    def withdraw(self):
+        amt=int(input("Enter your withdraw amount: "))
+        if amt%100==0:
+            if amt<=self.acbal:
+                if amt<=20000:
+                    self.acbal=self.acbal-amt
+                    obj.numberOfNotes(amt)
+                    self.wcount=self.wcount+1
+                    print("Avaliable bal is: ",self.acbal) 
+                else:
+                    print("Transaction limit is 20k only")
+            else:
+                print("Insuffcient fund")
+        else:
+            print("Please enter multiples of 100")
+    def deposit(self):
+        amt=int(input("Enter your deposite amount :"))
+        if amt%100==0:
+            if amt<=50000:
+                self.acbal=self.acbal+amt
+            else:
+                print("Transaction limit is 50k only")
+        else:
+            print("Please enter multiples of 100")
+        print("Avaliable bal is: ",self.acbal) 
+        
+        
+    def viewOptions(self):
+        print("Welcome to ABC bank ")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Balance Enquiry")
+        print("0. EXIT")
+        option=int(input("Choose your option: "))
+        if option==1:
+            obj.deposit()
+            obj.continue1()
+        elif option==2:
+            if self.wcount<3:
+                obj.withdraw()
+            else:
+                print("your transaction limit is over for the day")
+            obj.continue1()
+        elif option==3:
+            print("Avaliable bal is: ",self.acbal)
+            obj.continue1()
+        elif option==0:
+            print("Thank you, Visit again")
+        else:
+            print("Please select correct option ")
+        
+            
+
+obj=ATM()
+obj.validation()
+
+        
+        
